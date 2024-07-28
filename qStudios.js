8bb4540be51df30271789af73d28fc8b69d14362
Rafat Siddiqui <74584041+rafatsiddiqui9@users.noreply.github.com>
2024-07-28 21:15:52 +0530

    Update qStudios.js

--- qStudios.js
+++ qStudios.js
@@ -1,8 +1,3 @@
-import * as THREE from "three";
-import Stats from "three/addons/libs/stats.module.js";
-import { GUI } from "three/addons/libs/lil-gui.module.min.js";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-
 let group;
 let container, stats;
 const particlesData = [];
